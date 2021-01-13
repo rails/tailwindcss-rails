468d9b0d758e6a593b2de244af335250eab4f87a
David Heinemeier Hansson <david@loudthinking.com>
2021-01-13 14:42:13 +0100

    Fix tests to new API

--- test/purger_test.rb
+++ test/purger_test.rb
@@ -1,13 +1,16 @@
 require "test_helper"
 
 class Tailwindcss::PurgerTest < ActiveSupport::TestCase
-  test "basic purge" do
-    purger = Tailwindcss::Purger.new(
-      stylesheet_path: Pathname.new(__FILE__).join("../../app/assets/stylesheets/tailwindcss/tailwind.css"),
-      paths_with_css_class_names: [ "app/views/**/*.html*", "app/helpers/**/*.rb" ]
-    )
+  test "extract class names" do
+    assert_equal %w[ div class max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 ].sort,
+      %(<div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">).scan(Tailwindcss::Purger::CLASS_NAME_PATTERN).flatten.sort
+  end
 
-    assert_not purger.purge =~ /.mt-6 \{/
-    assert purger.purge =~ /.mt-10 \{/
+  test "basic purge" do
+    purger = Tailwindcss::Purger.new(paths_with_css_class_names: [ "app/views/**/*.html*", "app/helpers/**/*.rb" ])
+    purged = purger.purge(Pathname.new(__FILE__).join("../../app/assets/stylesheets/tailwind.css").read)
+
+    assert_not purged =~ /.mt-6 \{/
+    assert purged =~ /.mt-10 \{/
   end
 end
