a66290f08611dde151fe37df6468f3228a01af9f
David Heinemeier Hansson <david@loudthinking.com>
2021-01-19 12:23:15 +0100

    Cleanup webpack installer a bit

--- lib/install/tailwindcss_with_webpacker.rb
+++ lib/install/tailwindcss_with_webpacker.rb
@@ -1,24 +1,15 @@
-LATEST_WEBPACKER = "\"@rails\/webpacker\": \"rails\/webpacker#b6c2180\",".freeze
-TAILWIND_IMPORTS = "@import \"tailwindcss/base\";\n@import \"tailwindcss/components\";\n@import \"tailwindcss/utilities\";\n".freeze
-
-say "Installing Tailwind CSS"
+LATEST_WEBPACKER         = "\"@rails\/webpacker\": \"rails\/webpacker#b6c2180\","
+WEBPACK_STYLESHEETS_PATH = "#{Webpacker.config.source_path}/stylesheets"
 
 # Current webpacker version relies on an older version of PostCSS
 # which the latest TailwindCSS version is not compatible with
-gsub_file('package.json', /\"@rails\/webpacker\".*/) { |matched_line| matched_line = LATEST_WEBPACKER }
+gsub_file("package.json", /\"@rails\/webpacker\".*/) { |matched_line| matched_line = LATEST_WEBPACKER }
 
-say "Adding latest tailwind and postcss"
+say "Adding latest Tailwind CSS and postCSS"
 run "yarn add tailwindcss@latest postcss@latest autoprefixer@latest"
-insert_into_file "#{Webpacker.config.source_entry_path}/application.js",
-                 "\nrequire(\"stylesheets/application.scss\")\n"
+insert_into_file "#{Webpacker.config.source_entry_path}/application.js", "\nrequire(\"stylesheets/application.scss\")\n"
 
-say "Adding minimal configuration for TailwindCSS to work properly"
-stylesheets_directory = "#{Webpacker.config.source_path}/stylesheets"
-empty_directory stylesheets_directory
-add_file "#{stylesheets_directory}/application.scss"
-insert_into_file "#{stylesheets_directory}/application.scss", TAILWIND_IMPORTS
+say "Adding minimal configuration for Tailwind CSS to work properly"
+directory "stylesheets/application.scss", Webpacker.config.source_path
 
-insert_into_file "postcss.config.js",
-                 "require('tailwindcss'),\n\t", before: "require('postcss-import')"
-
-say "Tailwind CSS successfully installed️", :green
+insert_into_file "postcss.config.js", "require('tailwindcss'),\n\t", before: "require('postcss-import')"
