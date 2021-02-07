e2786d7b521700b6a6e4ad533a968217d36fc712
Wolfram Nikolas Müller <muellerwolfram@gmail.com>
2021-02-07 19:57:55 +0100

    Add tests and fixtures for haml support

--- test/purger_test.rb
+++ test/purger_test.rb
@@ -11,8 +11,26 @@ class Tailwindcss::PurgerTest < ActiveSupport::TestCase
       Tailwindcss::Purger.extract_class_names_from(Pathname.new(__dir__).join("fixtures/simple.html.erb"))
   end
 
+  test "extract class names from haml string" do
+    assert_equal %w[ class max-w-7xl mx-auto my-1.5 px-4 sm:px-6 lg:px-8 sm:py-0.5 translate-x-1/2 ].sort,
+      Tailwindcss::Purger.extract_class_names(%(.max-w-7xl.mx-auto.px-4.sm:px-6.lg:px-8{:class => "my-1.5 sm:py-0.5 translate-x-1/2"}))
+  end
+
   test "basic purge" do
-    purged = purged_tailwind_from_fixtures
+    purged = purged_tailwind_from_erb_fixtures
+
+    assert purged !~ /.mt-6 \{/
+
+    assert purged =~ /.mt-5 \{/
+    assert purged =~ /.sm\\:px-6 \{/
+    assert purged =~ /.translate-x-1\\\/2 \{/
+    assert purged =~ /.mt-10 \{/
+    assert purged =~ /.my-1\\.5 \{/
+    assert purged =~ /.sm\\:py-0\\.5 \{/
+  end
+
+  test "basic haml purge" do
+    purged = purged_tailwind_from_haml_fixtures
 
     assert purged !~ /.mt-6 \{/
 
@@ -39,19 +57,19 @@ class Tailwindcss::PurgerTest < ActiveSupport::TestCase
   end
 
   test "purge removes empty blocks" do
-    purged = purged_tailwind_from_fixtures
+    purged = purged_tailwind_from_erb_fixtures
 
     assert purged !~ /\{\s*\}/
   end
 
   test "purge removes top-level comments" do
-    purged = purged_tailwind_from_fixtures
+    purged = purged_tailwind_from_erb_fixtures
 
     assert purged !~ /^#{Regexp.escape "/*"}/
   end
 
   test "purge shouldn't remove hover or focus classes" do
-    purged = purged_tailwind_from_fixtures
+    purged = purged_tailwind_from_erb_fixtures
     assert purged =~ /.hover\\\:text-gray-500\:hover \{/
     assert purged =~ /.focus\\\:outline-none\:focus \{/
     assert purged =~ /.focus-within\\\:outline-black\:focus-within \{/
@@ -142,8 +160,12 @@ class Tailwindcss::PurgerTest < ActiveSupport::TestCase
       purged_css(tailwind, keep_these_class_names: keep_these_class_names)
     end
 
-    def purged_tailwind_from_fixtures
-      $purged_tailwind_from_fixtures ||= purged_tailwind_from Pathname(__dir__).glob("fixtures/*.html.erb")
+    def purged_tailwind_from_erb_fixtures
+      $purged_tailwind_from_erb_fixtures ||= purged_tailwind_from Pathname(__dir__).glob("fixtures/*.html.erb")
+    end
+
+    def purged_tailwind_from_haml_fixtures
+      $purged_tailwind_from_haml_fixtures ||= purged_tailwind_from Pathname(__dir__).glob("fixtures/*.html.haml")
     end
 
     def purged_tailwind_from files
