0799610f37cddaaa3d17e379d30536588965e333
Gannon McGibbon <gannon.mcgibbon@gmail.com>
2021-08-08 02:28:05 -0400

    Skip sprockets initializers for apps that don't use sprockets

--- lib/tailwindcss/engine.rb
+++ lib/tailwindcss/engine.rb
@@ -2,22 +2,24 @@ require "tailwindcss/compressor"
 
 module Tailwindcss
   class Engine < ::Rails::Engine
-    initializer "tailwindcss.compressor" do
-      Sprockets.register_compressor "text/css", :purger, Tailwindcss::Compressor
-    end
+    if defined?(Sprockets)
+      initializer "tailwindcss.compressor" do
+        Sprockets.register_compressor "text/css", :purger, Tailwindcss::Compressor
+      end
 
-    initializer "tailwindcss.assets" do
-      Rails.application.config.assets.precompile += %w( tailwind.css inter-font.css )
-    end
+      initializer "tailwindcss.assets" do
+        Rails.application.config.assets.precompile += %w( tailwind.css inter-font.css )
+      end
 
-    initializer "tailwindcss.disable_generator_stylesheets" do
-      Rails.application.config.generators.stylesheets = false
-    end
+      initializer "tailwindcss.disable_generator_stylesheets" do
+        Rails.application.config.generators.stylesheets = false
+      end
 
-    initializer "tailwindcss.disable_assets_cache" do
-      Rails.application.config.assets.configure do |env|
-        env.cache = ActiveSupport::Cache.lookup_store(:null_store)
-      end if Rails.env.production?
+      initializer "tailwindcss.disable_assets_cache" do
+        Rails.application.config.assets.configure do |env|
+          env.cache = ActiveSupport::Cache.lookup_store(:null_store)
+        end if Rails.env.production?
+      end
     end
   end
 end
