aeae1a9f67983d5bba430e74747fb9782877eea5
David Heinemeier Hansson <david@loudthinking.com>
2021-08-26 12:32:45 +0200

    Rails 7 won't have this by default, so decrease the noise

--- lib/install/tailwindcss.rb
+++ lib/install/tailwindcss.rb
@@ -8,8 +8,10 @@ else
   say %(        Add <%= stylesheet_link_tag "inter-font", "data-turbo-track": "reload" %> and <%= stylesheet_link_tag "tailwind", "data-turbo-track": "reload" %> within the <head> tag in your custom layout.)
 end
 
-say "Removing scaffold styles"
-remove_file Rails.root.join("app/assets/stylesheets/scaffolds.scss")
+# No longer included by default in Rails 7, but for earlier versions of Rails
+if (scaffolds_css_path = Rails.root.join("app/assets/stylesheets/scaffolds.scss")).exist?
+  remove_file scaffolds_css_path
+end
 
 say "Turn on purging of unused css classes in production"
 gsub_file Rails.root.join("config/environments/production.rb"), /^\s+#?\s+config.assets.css_compressor =.*$/, %(  config.assets.css_compressor = :purger)
