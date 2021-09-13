9c5aaa38086beffc5b423e312d3236ef91f049c4
Dino Maric <dino.onex@gmail.com>
2021-09-13 21:03:41 +0200

    Remove commented out code from engine.rb

--- lib/tailwindcss/engine.rb
+++ lib/tailwindcss/engine.rb
@@ -21,7 +21,6 @@ module Tailwindcss
     end
 
      config.app_generators do |g|
-       #g.templates.unshift File::expand_path('../../templates', __FILE__)
        g.template_engine :tailwindcss
      end
   end
