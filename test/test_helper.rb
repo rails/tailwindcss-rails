47881fe23d07af1af3c765e618ed7e8cd23b83e2
Dino Maric <dino.onex@gmail.com>
2021-09-26 14:15:18 +0200

    Use `file_fixture` for file fixtures (#75)

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,3 +9,6 @@ require_relative "../lib/tailwindcss-rails"
 require "rails/test_unit/reporter"
 Rails::TestUnitReporter.executable = 'bin/test'
 
+class ActiveSupport::TestCase
+  self.file_fixture_path = File.expand_path("fixtures/files", __dir__)
+end
