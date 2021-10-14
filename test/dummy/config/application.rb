24f7b8c6733929debb6ac14c96ccd78a9d405381
facundo espinosa <facundo.espinosa@fing.edu.uy>
2021-10-13 21:59:35 -0500

    Delete comments and unnecesary files

--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -8,8 +8,6 @@ require "action_controller/railtie"
 require "action_view/railtie"
 require "sprockets/railtie"
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 require "tailwindcss-rails"
 
@@ -18,16 +16,5 @@ module Dummy
     config.load_defaults Rails::VERSION::STRING.to_f
 
     config.paths["app/views"].unshift("#{Rails.root}/app/custom_views")
-
-    # Configuration to set custom_views as another files_with_class_names for Tailwindcss
-    # config.tailwind.files_with_class_names += Rails.root.glob("app/custom_views/**/*.*")
-
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
   end
 end
