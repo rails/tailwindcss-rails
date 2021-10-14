0728ae11b69b2e7b1bd90d9b3573be98ce5d1031
facundo espinosa <facundo.espinosa@fing.edu.uy>
2021-10-13 21:43:02 -0500

    Remove ActionCable

--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -1,6 +1,13 @@
 require_relative "boot"
 
-require "rails/all"
+require "active_model/railtie"
+require "active_job/railtie"
+require "active_record/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_view/railtie"
+require "sprockets/railtie"
+require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
