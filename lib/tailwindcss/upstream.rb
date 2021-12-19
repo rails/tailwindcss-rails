251b4fff2e5dca994061201e14117e3c9810e14d
David Heinemeier Hansson <david@hey.com>
2021-12-19 12:18:02 -0800

    Bump for Tailwind 3.0.7

--- lib/tailwindcss/upstream.rb
+++ lib/tailwindcss/upstream.rb
@@ -1,7 +1,7 @@
 module Tailwindcss
   # constants describing the upstream tailwindcss project
   module Upstream
-    VERSION = "v3.0.5"
+    VERSION = "v3.0.7"
 
     # rubygems platform name => upstream release filename
     NATIVE_PLATFORMS = {
