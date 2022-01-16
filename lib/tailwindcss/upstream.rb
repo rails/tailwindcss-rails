ad384c73fc49879f497f9b5b9dcd349858d052b2
David Heinemeier Hansson <david@hey.com>
2022-01-16 10:59:07 +0100

    Upgrade to Tailwind CSS v3.0.15

--- lib/tailwindcss/upstream.rb
+++ lib/tailwindcss/upstream.rb
@@ -1,7 +1,7 @@
 module Tailwindcss
   # constants describing the upstream tailwindcss project
   module Upstream
-    VERSION = "v3.0.8"
+    VERSION = "v3.0.15"
 
     # rubygems platform name => upstream release filename
     NATIVE_PLATFORMS = {
