0193dd70176b1743d9589e47b48cc7ff0adbc06c
Duke <emersonalmeidax@gmail.com>
2022-02-19 06:11:01 -0300

    Update tailwindcss to v3.0.22 (#143)
    # CHANGELOG
    
    ## [3.0.22] - 2022-02-11
    
    ### Fixed
    
    - Temporarily move postcss to dependencies ([#7424](https://github.com/tailwindlabs/tailwindcss/pull/7424))
    
    ## [3.0.21] - 2022-02-10
    
    ### Fixed
    
    - Move prettier plugin to dev dependencies ([#7418](https://github.com/tailwindlabs/tailwindcss/pull/7418))
    
    ## [3.0.20] - 2022-02-10
    
    ### Added
    
    - Expose `context.sortClassList(classes)` ([#7412](https://github.com/tailwindlabs/tailwindcss/pull/7412))
    
    ## [3.0.19] - 2022-02-07
    
    ### Fixed
    
    - Fix preflight border color fallback ([#7288](https://github.com/tailwindlabs/tailwindcss/pull/7288))
    - Correctly parse shadow lengths without a leading zero ([#7289](https://github.com/tailwindlabs/tailwindcss/pull/7289))
    - Don't crash when scanning extremely long class candidates ([#7331](https://github.com/tailwindlabs/tailwindcss/pull/7331))
    - Use less hacky fix for URLs detected as custom properties ([#7275](https://github.com/tailwindlabs/tailwindcss/pull/7275))
    - Correctly generate negative utilities when dash is before the prefix ([#7295](https://github.com/tailwindlabs/tailwindcss/pull/7295))
    - Detect prefixed negative utilities in the safelist ([#7295](https://github.com/tailwindlabs/tailwindcss/pull/7295))
    
    ## [3.0.18] - 2022-01-28
    
    ### Fixed
    
    - Fix `@apply` order regression (in `addComponents`, `addUtilities`, ...) ([#7232](https://github.com/tailwindlabs/tailwindcss/pull/7232))
    - Quick fix for incorrect arbitrary properties when using URLs ([#7252](https://github.com/tailwindlabs/tailwindcss/pull/7252))
    
    ## [3.0.17] - 2022-01-26
    
    ### Fixed
    
    - Remove false positive warning in CLI when using the `--content` option ([#7220](https://github.com/tailwindlabs/tailwindcss/pull/7220))
    
    ## [3.0.16] - 2022-01-24
    
    ### Fixed
    
    - Ensure to transpile the PostCSS Nesting plugin (tailwindcss/nesting) ([#7080](https://github.com/tailwindlabs/tailwindcss/pull/7080))
    - Improve various warnings ([#7118](https://github.com/tailwindlabs/tailwindcss/pull/7118))
    - Fix grammatical mistake ([cca5a38](https://github.com/tailwindlabs/tailwindcss/commit/cca5a3804e1d3ee0214491921e1aec35bf62a813))

--- lib/tailwindcss/upstream.rb
+++ lib/tailwindcss/upstream.rb
@@ -1,7 +1,7 @@
 module Tailwindcss
   # constants describing the upstream tailwindcss project
   module Upstream
-    VERSION = "v3.0.15"
+    VERSION = "v3.0.22"
 
     # rubygems platform name => upstream release filename
     NATIVE_PLATFORMS = {
