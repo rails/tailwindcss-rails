9f0659adf728de4872f3dede77200142da3dbfd4
Dino Maric <dino.onex@gmail.com>
2022-12-03 18:00:27 +0100

    Fix generating nested models
    Tis PR fixes error when generating nested models such as:
    
    `bin/rails g scaffold admin/role name description`

--- test/lib/generators/tailwindcss/scaffold_generator_test.rb
+++ test/lib/generators/tailwindcss/scaffold_generator_test.rb
@@ -12,4 +12,10 @@ class Tailwindcss::Generators::ScaffoldGeneratorTest < Rails::Generators::TestCa
 
     %w(index edit new show _form _message).each { |view| assert_file "app/views/messages/#{view}.html.erb" }
   end
+
+  test "with namespace invoked" do
+    run_generator [ "admin/role", "name:string", "description:string" ]
+
+    %w(index edit new show _form _role).each { |view| assert_file "app/views/admin/roles/#{view}.html.erb" }
+  end
 end
