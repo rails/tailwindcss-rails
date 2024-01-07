f989541ccfd8029fd0fdc967bb50ee6d62e8b51c
Mike Dalessio <mike.dalessio@gmail.com>
2024-01-07 09:54:04 -0500

    dep: update tailwindcss to v3.4.1
    https://github.com/tailwindlabs/tailwindcss/releases/tag/v3.4.1

--- lib/tailwindcss/upstream.rb
+++ lib/tailwindcss/upstream.rb
@@ -1,7 +1,7 @@
 module Tailwindcss
   # constants describing the upstream tailwindcss project
   module Upstream
-    VERSION = "v3.4.0"
+    VERSION = "v3.4.1"
 
     # rubygems platform name => upstream release filename
     NATIVE_PLATFORMS = {
