c0e1e52b223bfa9b3748ab6c1e50a84ccd0b818f
Jerome Dalbert <jerome.dalbert@gmail.com>
2024-07-30 07:07:43 -0700

    Force bin/dev overwrite to deal with new default bin/dev file (#385)
    See Rails https://github.com/rails/rails/pull/52433

--- lib/install/tailwindcss.rb
+++ lib/install/tailwindcss.rb
@@ -49,7 +49,7 @@ else
 end
 
 say "Add bin/dev to start foreman"
-copy_file "#{__dir__}/dev", "bin/dev"
+copy_file "#{__dir__}/dev", "bin/dev", force: true
 chmod "bin/dev", 0755, verbose: false
 
 say "Compile initial Tailwind build"
