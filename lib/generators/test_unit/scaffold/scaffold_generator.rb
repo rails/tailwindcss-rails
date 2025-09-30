14767db03e4bb7669da15ae51e094af5fadc7215
Emmanuel Hayford <hi@hayford.dev>
2025-09-30 07:49:02 +0200

    Fix system tests

--- lib/generators/test_unit/scaffold/scaffold_generator.rb
+++ lib/generators/test_unit/scaffold/scaffold_generator.rb
@@ -4,7 +4,7 @@ module TestUnit # :nodoc:
   module Generators # :nodoc:
     class ScaffoldGenerator < Base # :nodoc:
       def fix_system_test
-        return unless turbo_defined? && options[:system_tests] == "true"
+        return unless turbo_defined? && options[:system_tests]
         test_file = File.join("test/system", class_path, "#{file_name.pluralize}_test.rb")
         return unless File.exist?(test_file)
         gsub_file test_file, /(click_on.*Destroy this.*)$/, "accept_confirm { \\1 }"
