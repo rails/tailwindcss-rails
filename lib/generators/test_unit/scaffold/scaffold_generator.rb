9f020364660626a27fb709ed62d95a6e82ead8e5
Emmanuel Hayford <hi@hayford.dev>
2025-09-30 07:35:17 +0200

    Fix system test for scaffolds
    Fixes #585

--- lib/generators/test_unit/scaffold/scaffold_generator.rb
+++ lib/generators/test_unit/scaffold/scaffold_generator.rb
@@ -4,11 +4,10 @@ module TestUnit # :nodoc:
   module Generators # :nodoc:
     class ScaffoldGenerator < Base # :nodoc:
       def fix_system_test
-        if turbo_defined? && options[:system_tests]
-          gsub_file File.join("test/system", class_path, "#{file_name.pluralize}_test.rb"),
-                    /(click_on.*Destroy this.*)$/,
-                    "accept_confirm { \\1 }"
-        end
+        return unless turbo_defined? && options[:system_tests] == "true"
+        test_file = File.join("test/system", class_path, "#{file_name.pluralize}_test.rb")
+        return unless File.exist?(test_file)
+        gsub_file test_file, /(click_on.*Destroy this.*)$/, "accept_confirm { \\1 }"
       end
 
       private
