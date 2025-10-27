770b8e90668f7a3b14aebeae8fb38188bd050d15
Mike Dalessio <mike@37signals.com>
2025-10-27 15:40:50 -0400

    Gracefully handle the new Rails default of "no system tests"
    Fixes #585

--- lib/generators/test_unit/scaffold/scaffold_generator.rb
+++ lib/generators/test_unit/scaffold/scaffold_generator.rb
@@ -4,10 +4,9 @@ module TestUnit # :nodoc:
   module Generators # :nodoc:
     class ScaffoldGenerator < Base # :nodoc:
       def fix_system_test
-        if turbo_defined? && options[:system_tests]
-          gsub_file File.join("test/system", class_path, "#{file_name.pluralize}_test.rb"),
-                    /(click_on.*Destroy this.*)$/,
-                    "accept_confirm { \\1 }"
+        system_test_file = File.join("test/system", class_path, "#{file_name.pluralize}_test.rb")
+        if turbo_defined? && options[:system_tests] && File.exist?(system_test_file)
+          gsub_file(system_test_file, /(click_on.*Destroy this.*)$/, "accept_confirm { \\1 }")
         end
       end
 
